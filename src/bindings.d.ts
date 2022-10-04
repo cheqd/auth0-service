862f765f2da50a5ac6a36da013b8aaad4c9cacf3
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2022-10-04 19:14:14 +0100

    Update bindings.d.ts

--- src/bindings.d.ts
+++ src/bindings.d.ts
@@ -2,16 +2,10 @@ export {}
 
 declare global {
     const _AUTH0_DOMAIN: string
-
     const _AUTH0_CLIENT_ID: string
-
     const _AUTH0_REDIRECT_URI: string
-
     const _AUTH0_URI: string
-
     const _TWITTER_FETCH_USER_URI: string
-
     const _TWITTER_BEARER_TOKEN: string
-
     const CACHE: KVNamespace
 }
